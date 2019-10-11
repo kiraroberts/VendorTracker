f328989ff968d1f286d8fd22c6a1f4e26eeb8d30
Kira Roberts <rosehill1788@protonmail.com>
2019-10-11 16:16:48 -0700

    add route to show vendors by id

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -31,5 +31,15 @@ namespace VendorTracker.Controllers
             model.Add("vendor", foundVendor);
             return View("Show", model);
         }
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show (int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = selectedVendor.Orders;
+            model.Add("vendor", selectedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
+        }
     }
 }
