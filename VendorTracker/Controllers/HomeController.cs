e864351321855dee60894b9f5c8d3dd3505ece02
Kira Roberts <rosehill1788@protonmail.com>
2019-10-11 12:58:06 -0700

    add Program.cs, Startup.cs, VendorTracker.csproj, code to HomeController

---
+++ VendorTracker/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendorTracker.Controllers
+{
+    public class HomeController : Controllers
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
